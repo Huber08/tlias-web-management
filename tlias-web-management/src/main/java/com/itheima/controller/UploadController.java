0074f441dcc291347d91c34a0a6adc0b4935a7b5
Cosmos <3160536094@qq.com>
2026-04-27 23:21:57 +0800

    Initial commit: Tlias web management project

---
+++ tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
@@ -0,0 +1,35 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Result;
+import com.itheima.utils.AliyunOSSOperator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+public class UploadController {
+
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("上传文件：{}", file.getOriginalFilename());
+
+        if (file.isEmpty()) {
+            return Result.error("上传失败：文件为空");
+        }
+
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename == null || originalFilename.isEmpty()) {
+            return Result.error("上传失败：文件名无效");
+        }
+
+        String url = aliyunOSSOperator.upload(file.getBytes(), originalFilename);
+        return Result.success(url);
+    }
+
+}
